77d9e645774e275c9fe2d649d3eb695e3f85d29f
Sam Dufel <sdufel@undergroundelephant.com>
2014-09-08 09:53:52 -0700

    Moved card ID to parameter

--- src/Message/SaleRequest.php
+++ src/Message/SaleRequest.php
@@ -19,9 +19,9 @@ class SaleRequest extends AbstractRequest
     {
         $trans = new Sale($this->getProfileId(), $this->getProfileKey());
         $trans->setAvsRequest($this->getCard()->getAddress1(), $this->getCard()->getPostcode());
-        $trans->setStoredData($this->getCardId(), $this->getAmountInteger());
-        $trans->setRequestField('card_exp_date',$this->getCard()->getExpiryDate('Ym'));
-        $trans->setRequestField('invoice_number',$this->getInvoiceNumber());
+        $trans->setStoredData($this->parameters->get('card_id'), $this->getAmount());
+        $trans->setRequestField('card_exp_date', $this->getCard()->getExpiryDate('Ym'));
+        $trans->setRequestField('invoice_number', $this->parameters->get('invoice_number'));
         $trans->setHost($this->host);
         $trans->execute();
 
